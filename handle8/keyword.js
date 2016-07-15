87da06cbf07087a273e7c634fc90eb7ac3fd7ff6
royhyang <yhsunshining@gmail.com>
2016-07-15 16:15:27 +0800

    add redis

--- handle8/keyword.js
+++ handle8/keyword.js
@@ -6,9 +6,16 @@ var redisClient = require('../lib/redis');
 
 router.get('/',function(req,res){
     const params = request.query;
-    const keyword = params.search;
+    const keyword = params.search || '';
+    let pageIndex = +params.pageIndex;
+    let pageSize = +params.pageSize;
+
     redisClient.on('connect', function(){
-        
-    })
-    res.send(keyword);
+        if(redisClient.llen(keyword)>0) {
+            res.send(redisClient.lrange(keyword,pageIndex*pageSize,pageSize))
+        }
+        else {
+            res.send(keyword);
+        }
+    });
 });
