6eb57f25ef0a3171a54269dec7ed35c1ffb2f8d1
royhyang <yhsunshining@gmail.com>
2016-07-16 21:17:10 +0800

    add redis config

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -2,10 +2,10 @@
  * Created by royhyang on 2016/7/16.
  */
 module.exports = {
-    ip : '127.0.0.1',
+    // ip : '127.0.0.1',
     ip : '10.66.123.211',
     port : 6379,
     options: {
-        // auth_pass:'crs-9snxiqbn:QcloudV5'
+        auth_pass:'crs-9snxiqbn:QcloudV5'
     }
 }
