e1f9264c8eaacbea3f13edead6a551e3afd38db1
royhyang <yhsunshining@gmail.com>
2016-07-17 20:15:58 +0800

    STEP4.请求搜索API前校验缓存信息

--- server.js
+++ server.js
@@ -21,25 +21,29 @@ app.use('/', function(req, res) {
         res.status(404).send('Not found:' + req.originalUrl);
         return;
     }
-    // 设置转发url
-    var targetUrl = 'http://' + proxyConfig[pathArr[1]].host + ':' + proxyConfig[pathArr[1]].port + req.originalUrl.replace(/\/part\d/, '');
-    // 重置请求方式
-    req.method = routesConfig[req.path].method;
-    // 不能使用bodyParser，会把req里数据流进行更改，对pipe方法造成影响
-    req.pipe(request(targetUrl)).on('error', function(err) {
-        // 处理目标服务器错误
-        res.status(404).send('Not found:' + req.originalUrl);
-        return;
-    }).on('response', function(response) {
-        // redis缓存处理
-        var bodyChunks = [];
-        response.on('data', function(chunk) {
-            bodyChunks.push(chunk);
-        }).on('end', function() {
-            var body = Buffer.concat(bodyChunks);
-            //*****do somethings
-        });
-    }).pipe(res);
+    require('/handle/list').checkRedis(req,res,function() {
+        // 设置转发url
+        var targetUrl = 'http://' + proxyConfig[pathArr[1]].host + ':' + proxyConfig[pathArr[1]].port + req.originalUrl.replace(/\/part\d/, '');
+        // 重置请求方式
+        req.method = routesConfig[req.path].method;
+        // 不能使用bodyParser，会把req里数据流进行更改，对pipe方法造成影响
+        req.pipe(request(targetUrl)).on('error', function(err) {
+            // 处理目标服务器错误
+            console.log('target server error');
+            res.status(404).send('Not found:' + req.originalUrl);
+            return;
+        }).on('response', function(response) {
+            // redis缓存处理
+            var bodyChunks = [];
+            response.on('data', function(chunk) {
+                bodyChunks.push(chunk);
+            }).on('end', function() {
+                var body = Buffer.concat(bodyChunks);
+                require('./handel/list').addRedisALL(req,res,body);
+                console.log('cache');
+            });
+        }).pipe(res);
+    });
 });
 
 server.listen(3008);
