8d0dc59d2748481d815578b7769cefdf936dc5e1
royhyang <yhsunshining@gmail.com>
2016-07-17 20:03:18 +0800

    STEP0.node环境初始化

---
+++ config/authConfig.js
@@ -0,0 +1,10 @@
+/**
+ * 权限设置
+ * @author qcloud
+ * @date   2016.7.4
+ */
+module.exports = {
+	'qcloud':'123456',
+	'qcbuy':'654321',
+	'test':'test'
+}
---
+++ routes/index.js
@@ -0,0 +1,56 @@
+﻿/**
+ * 路由配置
+ * @author     qcloud
+ * @date 	   2016.7.9
+ */
+
+module.exports = {
+	'/part0': {
+		'method': 'get',
+		'path': '/'
+	},
+	'/part1': {
+		'method': 'get',
+		'path': '/'
+	},
+	'/part4/upload': {
+		'method': 'post',
+		'path': '/upload'
+	},
+	'/part4/list': {
+		'method': 'get',
+		'path': '/list'
+	},
+	'/part4/meta': {
+		'method': 'put',
+		'path': '/meta'
+	},
+	'/part4/keyword': {
+		'method': 'get',
+		'path': '/keyword'
+	},
+	'/part4/delete': {
+		'method': 'delete',
+		'path': '/delete'
+	},
+	'/part5/upload': {
+		'method': 'post',
+		'path': '/upload'
+	},
+	'/part5/stat': {
+		'method': 'get',
+		'path': '/stat'
+	},
+	'/part5/resize': {
+		'method': 'put',
+		'path': '/resize'
+	},
+	'/part5/rotate': {
+		'method': 'get',
+		'path': '/rotate'
+	},
+	'/part5/delete': {
+		'method': 'delete',
+		'path': '/delete'
+	},
+}
