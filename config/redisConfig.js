5b050bc6c3acaef330527c6fa90cc0089439be31
royhyang <yhsunshining@gmail.com>
2016-07-17 19:47:34 +0800

    change config of redis

--- config/redisConfig.js
+++ config/redisConfig.js
@@ -3,17 +3,9 @@
  */
 module.exports = {
     ip : '127.0.0.1',
-    ip : '10.66.151.52',
+    // ip : '10.66.151.52',
     port : 6379,
     options: {
-        auth_pass:'crs-cbutvqq7:qcourse8'
-    }
-};
-
-module.exports = {
-    ip : '实例id',
-    port : '默认6379',
-    options: {
-        auth_pass:'实例id:密码'
+        // auth_pass:'crs-cbutvqq7:qcourse8'
     }
 };
--- server.js
+++ server.js
@@ -1,29 +1,66 @@
-'use strict';
+﻿var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var methodOverride = require('method-override');
+var request = require('request');
+var logger = require('morgan');
+var fs = require("fs");
+var proxyConfig = require('./config/proxyConfig');
+var routesConfig = require('./routes/index');
 
-const express = require("express");
-const bodyParser = require("body-parser");
+app.use(logger('dev'));
+app.use(require('./middlewares/basicAuth'));
+app.use(methodOverride());
 
-function start(port) {
-    const app = express();
-    
-    // tells the client the server can work.
-    app.get("/", (request, response) => {
-        response.write("It works!");
-        response.end();
-    });
+// 请求示例：http://127.0.0.1/part4/upload
+app.use('/', function(req, res) {
+    var pathArr = req.path.split('/');
+    console.log(pathArr);
+    // 针对不在配置文件中的路由进行错误处理
+    if (!proxyConfig[pathArr[1]] || !routesConfig[req.path]) {
+        console.log(proxyConfig[pathArr[1]]);
+        console.log(routesConfig[req.path]);
+        res.status(404).send('Not found:' + req.originalUrl);
+        return;
+    }
+    var beforeRequest = routesConfig[req.path].beforeRequest;
+    if(beforeRequest !== undefined){
+        beforeRequest(req,res,function(){
+            transport(req,res);
+        })
+    }
+    else {
+        transport(req,res);
+    }
+});
 
-    // 注意放在所有路由前面
-    app.use(bodyParser.json());
-    
-    app.use("/upload", require("./handle/upload"));
-    app.use("/list", require("./handle/list"));
-    app.use("/meta", require("./handle/meta"));
-    app.use("/keyword", require("./handle/keyword"));
-    app.use("/delete", require("./handle/delete"));
-
-    app.use("/redis/keyword", require("./handle/list"));
-
-    app.listen(port || 3008);
+function transport(req,res){
+    var pathArr = req.path.split('/');
+    // 设置转发url
+    var targetUrl = 'http://' + proxyConfig[pathArr[1]].host + ':' + proxyConfig[pathArr[1]].port + req.originalUrl.replace(/\/part\d/, '');
+    // 重置请求方式
+    req.method = routesConfig[req.path].method;
+    // 不能使用bodyParser，会把req里数据流进行更改，对pipe方法造成影响
+    req.pipe(request(targetUrl)).on('error', function(err) {
+        // 处理目标服务器错误
+        res.status(404).send('Not found:' + req.originalUrl);
+        return;
+    }).on('response', function(response) {
+        // redis缓存处理
+        console.log('cache');
+        var bodyChunks = [];
+        response.on('data', function(chunk) {
+            bodyChunks.push(chunk);
+        }).on('end', function() {
+            var body = Buffer.concat(bodyChunks);
+            var afterRequest = routesConfig[req.path].afterRequest;
+            if(afterRequest !== undefined){
+                afterRequest(req,res,body)
+            }
+            else {
+                return ;
+            }
+        });
+    }).pipe(res);
 }
-
-start();
+server.listen(3008);
